62b21b093cc5e1abb1132e0ccd1a8117150c4833
Realharshagarwal1 <realharshagarwal@gmail.com>
2026-03-01 18:09:39 +0530

    fix(build): add environment files required for production build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://www.vahanassist.com/vahaan-admin',
+    apiInsertUrl: 'https://www.vahanassist.com/vahaan-admin/Insert_con',
+    apiAppUrl: 'https://www.vahanassist.com/vahaan-admin/App',
+    apiPaymentUrl: 'https://www.vahanassist.com/vahaan-admin/Payment',
+    imageBaseUrl: 'https://www.vahanassist.com/vahaan-admin/images/vehicle_image/',
+    imageProfileUrl: 'https://www.vahanassist.com/vahaan-admin/images/profile/',
+    googleMapsApiKey: 'AIzaSyB39Z-mhm2udO-plmGRgG4QOyX3UjqOqqo',
+};
