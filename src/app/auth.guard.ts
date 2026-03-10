dae800188396d28e6a8eecb2f2da75632f263bf6
Vanshyadav493 <vanshyadav5134@gmail.com>
2026-03-10 14:08:30 +0530

    restrict services access without login

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -9,7 +9,7 @@ export const authGuard: CanActivateFn = (route, state) => {
        return true;
     }
     else{
-      router.navigate(['/login']);
+      router.navigate(['/signup']);
         return false;
     }
 
