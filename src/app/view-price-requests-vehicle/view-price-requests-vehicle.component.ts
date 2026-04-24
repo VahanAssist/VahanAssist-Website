b90f78e423cee4ad761fa580b4fe382ceef08b07
Realharshagarwal1 <realharshagarwal@gmail.com>
2026-04-24 21:59:05 +0530

    Restore lost files: image modal UI, price requests logic, and dialog cross

--- src/app/view-price-requests-vehicle/view-price-requests-vehicle.component.ts
+++ src/app/view-price-requests-vehicle/view-price-requests-vehicle.component.ts
@@ -2,97 +2,53 @@ import { Component } from '@angular/core';
 import { WebapiService } from '../webapi.service';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-view-price-requests-vehicle',
   standalone: true,
-  imports: [CommonModule,NgxPaginationModule,RouterLink,FormsModule],
+  imports: [CommonModule, NgxPaginationModule, RouterLink, FormsModule],
   templateUrl: './view-price-requests-vehicle.component.html',
   styleUrl: './view-price-requests-vehicle.component.css'
 })
 
 export class ViewPriceRequestsVehicleComponent {
-  enquiryList:any;
-  filter:any={};
+  priceRequestList: any[] = [];
+  filter: any = {};
   p: any = 1;
-  userId: any;
-  total:any=0;
+  vehicleId: any;
+  total: any = 0;
 
-  constructor(private webapi: WebapiService){
+  constructor(private webapi: WebapiService, private activatedRoute: ActivatedRoute) {
     this.filter = {
-      userId:'',
-      start:1,
-      limit:10
+      vehicleId: '',
+      start: 1,
+      limit: 10
     };
 
-    this.userId = sessionStorage.getItem('userId');
+    this.vehicleId = this.activatedRoute.snapshot.paramMap.get('id');
 
-    this.getAllEnquiry(this.userId);
-  }
-
-  getAllEnquiry(id:any){
-    this.filter.userId = id;
-    this.webapi.getAllEnquiry(this.filter).subscribe((res: any) => {
-      console.log(res);
-      this.enquiryList = res.data;
-      this.total = res.total
-    })
-  }
-
-  updateEnquiryStatus(event:any,id:any){
-    let cn = confirm('Are you sure you want to Update');
-
-    if(cn){
-      let val ={
-        id:id,
-        status:event.target.value
-      }
-      this.webapi.updateEnquiryStatus(val).subscribe((res: any) => {
-        if(res.status == 'ok'){
-          alert('Enquiry status updated');
-          this.getAllEnquiry(this.userId);
-        }
-        else{
-          alert('Enquiry status update Error!!');
-          this.getAllEnquiry(this.userId);
-        }
-
-      });
+    if (this.vehicleId) {
+      this.getPriceRequests();
     }
-
   }
 
-  hideMPEnquiry(enqid:any){
-    let cn = confirm('Are you sure you want to Update');
-
-    if(cn){
-      let val = {
-        id:enqid,
-        hide:1
+  getPriceRequests() {
+    this.filter.vehicleId = this.vehicleId;
+    this.webapi.getPriceRequestsByVehicle(this.filter).subscribe((res: any) => {
+      if (res.status === 'success') {
+        this.priceRequestList = res.data;
+        this.total = res.total;
       }
-      this.webapi.updateEnquiryVisibilty(val).subscribe((res: any) => {
-        if(res.status == 'ok'){
-          alert('Enquiry status updated');
-          this.getAllEnquiry(this.userId);
-        }
-        else{
-          alert('Enquiry status update Error!!');
-          this.getAllEnquiry(this.userId);
-        }
-
-      })
-    }
-
+    });
   }
 
   onTableDataChange(event: any) {
     this.filter.start = event;
-    this.getAllEnquiry(this.userId);
+    this.getPriceRequests();
     this.p = event;
- }
+  }
 
 }
-
